665c3790a8e30bab16a0c9de64150cb2e0cb3736
Hans Preinfalk <hans.preinfalk.davila@gmail.com>
2026-05-02 17:44:18 -0700

    Initial Commit

---
+++ jarvis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jarvis
+//
+//  Created by Hans Preinfalk on 5/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jarvis/jarvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  jarvisApp.swift
+//  jarvis
+//
+//  Created by Hans Preinfalk on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct jarvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
